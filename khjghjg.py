d3f01c45b0286276f58101779f904f4204010fc5
junaalee <116074282+junaalee@users.noreply.github.com>
2023-01-18 09:47:12 -0500

    Update khjghjg.py

--- khjghjg.py
+++ khjghjg.py
@@ -6,9 +6,20 @@ def main():
         list.append(x)
     for i in range(0,len(list)):
         list[i] = piglatin(list[i])
+    list.sort()
+    for i in list:
+        print(i)
 
-def piglatin():
-
-
-def returnfinal(list):
-    return list.sort()
+def piglatin(word):
+    list=[]
+    for i in word:
+        list.append[i]
+    if list[0] not in "aeiouAEIOU":
+        list.append[list[0]]
+        list.remove[list[0]]
+    for i in "ay":
+        list.append[i]
+    string=""
+    for i in list:
+        string+=i
+    return string
