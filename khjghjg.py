e3cb86d16595e35ad5fa1228c4b7ab9a8ae4532d
junaalee <116074282+junaalee@users.noreply.github.com>
2023-01-18 09:35:40 -0500

    Update khjghjg.py

--- khjghjg.py
+++ khjghjg.py
@@ -1,15 +1,6 @@
 <<<<<<< HEAD
 nhjkl;.,mnbvftyuil;.,mnbvcxdrtyukl,mnbvcxdftyuiol;.,mnb vcdfrtyuiklkmnbvcxsdfrtyuiol;.,mn bvcdfrtyuik
 
-ghyjuki,kjhyhnjijmkm
-
-jjl
-
-kk'jj'
-n'n
-nonlocal
-
-
 
 
 
